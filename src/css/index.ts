70fb562e052dd3ca11f2760503abb2f963900eab
Jessica Hansson <jessica.hansson@hotmail.se>
2024-10-04 15:35:03 +0200

    small fix

--- src/css/index.ts
+++ src/css/index.ts
@@ -1,5 +1,5 @@
 export * from "./index.css";
 export * from "./Header.css";
-export * from "./HeroCard.css";
 export * from "./Footer.css";
 export * from "./MyTeam.css";
+export * from "./HeroCard.css";
