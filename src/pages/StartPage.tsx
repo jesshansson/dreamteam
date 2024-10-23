3fa2a2282831551b36527c7b35cecbcb2b82cddf
Jessica Hansson <jessica.hansson@hotmail.se>
2024-10-23 10:24:07 +0200

    added toggle button

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -8,6 +8,7 @@ export function StartPage() {
   const initialHero = useLoaderData() as IHero; // Hämta den första slumpmässiga hjälten från loadern ("randomHero" i loadern)
   const [currentHero, setCurrentHero] = useState<IHero>(initialHero); // Lokalt state för att hantera knappen som hämtar nya hjältar
   const [loading, setLoading] = useState(true); // Hantera laddningsindikatorn
+  const [showText, setShowText] = useState(true);
 
   // Använd useEffect för att stänga av loading när hjälten har laddats
   useEffect(() => {
@@ -24,11 +25,21 @@ export function StartPage() {
       <div className="welcome-msg ">
         <h1 className="shimmer-text">Welcome to Dream Team HQ!</h1>
 
-        <p>
-          Ready to unleash your inner hero (or villain)? It’s time to assemble your ultimate squad!
-          Create your own legendary characters or recruit from the world’s greatest heroes and
-          villains. Your dream team awaits — who will rise to the challenge?
-        </p>
+        <div className={`welcome-text ${showText ? "open" : "closed"}`}>
+          <p>
+            Ready to unleash your inner hero (or villain)? It’s time to assemble your ultimate
+            squad! Create your own legendary characters or recruit from the world’s greatest heroes
+            and villains. Your dream team awaits — who will rise to the challenge?
+          </p>
+        </div>
+
+        <button onClick={() => setShowText(!showText)} className="toggle-text-btn">
+          {showText ? (
+            <span className="material-symbols-outlined close">collapse_all</span>
+          ) : (
+            <span className="material-symbols-outlined open">expand_all</span>
+          )}
+        </button>
       </div>
       {currentHero ? (
         <HeroCard hero={currentHero} showAddToFavorites={true} showEditButton={false} />
